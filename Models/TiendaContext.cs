5e78caa036e1be86ad3344816805d5e0d68b5e95
CheNohMarco <47462280+CheNohMarco@users.noreply.github.com>
2020-06-17 18:40:46 -0500

    second commit

---
+++ Models/TiendaContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tienda.Models
+{
+    class TiendaContext:DbContext
+    {
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Venta> Ventas { get; set; }
+        public DbSet<Detalle> Detalles { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;database=tienda;user=root;password=");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Linq;
+using Tienda.Models;
+
+namespace Tienda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        public static void Menu()
+        {
+            Console.WriteLine("Menu");
+            Console.WriteLine("1) Buscar producto");
+            Console.WriteLine("2) Crear producto");
+            Console.WriteLine("3) Eliminar producto");
+            Console.WriteLine("4) Actualizar producto");
+            Console.WriteLine("0) Salir");
+
+            string opcion = Console.ReadLine();
+            switch (opcion)
+            {
+                case "1":
+                    BuscarProductos();
+                    break;
+                case "2":
+                    CrearProductos();
+                    break;
+                case "3":
+                    ActualizarProducto();
+                    break;
+                case "4":
+                    EliminarProducto();
+                    break;
+                case "0": return;
+            }
+            Menu();
+        }
+
+        public static void BuscarProductos()
+        {
+            Console.WriteLine("Buscar prodcutos");
+            Console.Write("Buscar: ");
+            string buscar = Console.ReadLine();
+
+            using (TiendaContext context = new TiendaContext())
+            {
+                IQueryable<Producto> productos = context.Productos.Where(p => p.Nombre.Contains(buscar));
+                foreach (Producto producto in productos)
+                {
+                    Console.WriteLine(producto);
+                }
+            }
+        }
+
+        public static void CrearProductos()
+        {
+            Console.WriteLine("Crear producto");
+            Producto producto = new Producto();
+            producto = LlenarProducto(producto);
+
+            using (TiendaContext context = new TiendaContext())
+            {
+                context.Add(producto);
+                context.SaveChanges();
+                Console.WriteLine("Producto creado exitosamente");
+            }
+        }
+
+        public static Producto LlenarProducto(Producto producto)
+        {
+            Console.Write("Nombre: ");
+            producto.Nombre = Console.ReadLine();
+            Console.Write("Descripcion: ");
+            producto.Descripcion = Console.ReadLine();
+            Console.Write("Precio: ");
+            producto.Precio = decimal.Parse(Console.ReadLine());
+            Console.Write("Costo: ");
+            producto.Costo = decimal.Parse(Console.ReadLine());
+            Console.Write("Cantidad: ");
+            producto.Cantidad = decimal.Parse(Console.ReadLine());
+            Console.Write("Tamaño: ");
+            producto.Tamano = Console.ReadLine();
+
+            return producto;
+        }
+
+        public static Producto SeleccionarProducto()
+        {
+            BuscarProductos();
+            Console.Write("Selecciona el código de producto: ");
+            uint id = uint.Parse(Console.ReadLine());
+
+            using (TiendaContext context = new TiendaContext())
+            {
+                Producto producto = context.Productos.Find(id);
+                if (producto == null)
+                {
+                    SeleccionarProducto();
+                }
+                return producto;
+            }
+        }
+
+        public static void ActualizarProducto()
+        {
+            Console.WriteLine("Actualizar producto");
+            Producto producto = SeleccionarProducto();
+            producto = LlenarProducto(producto);
+
+            using (TiendaContext context = new TiendaContext())
+            {
+                context.Update(producto);
+                context.SaveChanges();
+                Console.WriteLine("Producto actualizado exitosamente");
+            }
+        }
+
+        public static void EliminarProducto()
+        {
+            Console.WriteLine("Eliminar producto");
+            Producto producto = SeleccionarProducto();
+
+            using (TiendaContext context = new TiendaContext())
+            {
+                context.Remove(producto);
+                context.SaveChanges();
+                Console.WriteLine("Producto eliminado exitosamente");
+            }
+        }
+    }
+}
